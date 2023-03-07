b06943efea4d1133f81c74b6cb1a0f35566c9515
anxelic <chrisinfinityy@gmail.com>
2023-03-07 17:39:07 -0500

    scrapped abstraction

--- Game-Design/src/com/company/Main.java
+++ Game-Design/src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner s = new Scanner(System.in);
+        System.out.println("What class do you desire to be?: warrior, face, rogue, or mage (please write it as shown)");
+        String input = s.nextLine().toLowerCase();
+        s.close();
+
+        Player player = new Player(input);
+        player.listStats();
     }
 }
--- Game-Design/src/com/company/Player.java
+++ Game-Design/src/com/company/Player.java
@@ -1,25 +1,57 @@
 package com.company;
 
-public abstract class Player 
-{
-	
-	private Player _classes;
-	
-	protected Player(Player classes)
-	{
-		_classes = classes;
+import java.io.*;
+import java.util.Arrays;
+
+public class Player {
+	String spec;
+	int[] stats = new int[4];
+	//strength
+	//agility
+	//charisma
+	//intelligence
+
+	public Player(String s) {
+		spec = s;
+		stats = statDetermine();
 	}
-	
-	public abstract boolean strength();
-	{}
-	
-	public abstract boolean agility();
-	{}
-	
-	public abstract boolean charisma();
-	{}
-	
-	public abstract boolean intelligence();
-	{}
-	
+
+	private int[] statDetermine() {
+		int[] stats = new int[4];
+		switch (spec) {
+			case "warrior":
+				stats[0] = 25;
+				stats[1] = 7;
+				stats[2] = 9;
+				stats[3] = 1;
+				break;
+			case "face":
+				stats[0] = 5;
+				stats[1] = 3;
+				stats[2] = 25;
+				stats[3] = 9;
+				break;
+			case "rogue":
+				stats[0] = 9;
+				stats[1] = 25;
+				stats[2] = 3;
+				stats[3] = 5;
+				break;
+			case "mage":
+				stats[0] = 2;
+				stats[1] = 8;
+				stats[2] = 7;
+				stats[3] = 25;
+				break;
+		}
+		return stats;
+	}
+
+	public void listStats () {
+		System.out.println("Strength: " + stats[0]);
+		System.out.println("Agility: " + stats[1]);
+		System.out.println("Charisma: " + stats[2]);
+		System.out.println("Intelligence: " + stats[3]);
+	}
+
 }

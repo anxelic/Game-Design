9c0b11ecd3ca77fba4bd576c427889983db9a137
anxelic <chrisinfinityy@gmail.com>
2023-03-09 16:47:32 -0500

    added various functions and flushed player class

--- Game-Design/src/com/company/Main.java
+++ Game-Design/src/com/company/Main.java
@@ -5,11 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("What class do you desire to be?: warrior, face, rogue, or mage (please write it as shown)");
+        System.out.println("What class do you desire to be?: warrior, bard, rogue, or mage (please write it as shown)");
         String input = s.nextLine().toLowerCase();
-        s.close();
-
         Player player = new Player(input);
         player.listStats();
+        player.displayInventory();
+
+        player.command(s);
+
+        // System.out.println("What stat do you wish to increase? (strength, agility, charisma, intelligence):");
+        // input = s.nextLine().toLowerCase();
+        // player.upgradeStat(input);
+        // player.listStats();
+
+        // player.xpGain(250, s);
+        // player.listStats();
+
+        
     }
 }

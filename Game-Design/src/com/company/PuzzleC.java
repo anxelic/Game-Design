06731831664e65fb2c0bdac900b4e7b39702a9b6
anxelic <chrisinfinityy@gmail.com>
2023-04-27 14:20:13 -0400

    added nick's changes to puzzlec

--- Game-Design/src/com/company/PuzzleC.java
+++ Game-Design/src/com/company/PuzzleC.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class PuzzleC {
 
 	private static Scanner s = new Scanner(System.in);
+	private static String[] comb = {"", "", "", "", "", "", "", "", "", "", "", ""};
+	private static boolean[] correct = {false, false, false, false, false, false, false, false, false, false, false, false};
 	
 	public static void main(String[] args, Player p) {
 		System.out.println("You enter a room with a long hallway, filled with traps. In front of the hallway is a pannel.");
@@ -56,31 +58,45 @@ public class PuzzleC {
 	}
 	
 	public static void lockCombination(Player p) {
+		String[] correctComb = {"p", "a", "s", "s", "w", "o", "r", "d", "1", "2", "3", "4"};
         String combCode = "";
         if (p.stats[3] >= 10) {
             System.out.println("With your high intellect, you figure the code out instantly.");
             combCode = "password1234";
         } else {
-            String[] comb = {"", "", "", "", "", "", "", "", "", "", "", ""};
             for(int i = 0; i < 12; i++) {
-                System.out.println("What is the letter/number in spot number " + (i + 1) + "?");
-                String ans = s.nextLine().toLowerCase();
-                if(ans.length() > 1) {
-                    System.out.println("Sorry, only 1 input per lock combination, try again.");
-                    i--;
-                } else if(ans.length() < 1) {
-                    System.out.println("You need at least 1 combination, try again.");
-                    i--;
-                } else {
-                    comb[i] = ans;
-                    combCode = combCode + ans;
-                }
-            }
-        } 
+				if (correct[i] != true) {
+					System.out.println("What is the letter/number in spot number " + (i + 1) + "?");
+					String ans = s.nextLine().toLowerCase();
+					if(ans.length() > 1) {
+						System.out.println("Sorry, only 1 input per lock combination, try again.");
+						i--;
+					} else if(ans.length() < 1) {
+						System.out.println("You need at least 1 combination, try again.");
+						i--;
+					} else {
+						comb[i] = ans;
+						combCode = combCode + ans;
+					}
+				} else {
+					combCode = combCode + comb[i];
+				}
+			}      
+        }
+		for (int i = 0; i < 12; i++) {
+			if (comb[i].equals(correctComb[i])) {
+				correct[i] = true;
+			} else {
+				System.out.println("Combination spot " + (i + 1) + " is incorrect.");
+			}
+		}
         if(combCode.equals("password1234")) {
 			System.out.println("You tried to unlock the pannel, and it has stopped all of the traps down the hall. '" + combCode + "' was the correct password.");
 			System.out.println("Going down the hallway, you noticed that all of the traps are down and you made your way down the hall.");
 			//System.out.println("You have acquired the Blue Key.");
+			for (int i = 0; i < 12; i++) {
+				correct[i] = false;
+			}
             p.addItemToInventory("blue_key");
             p.blue_key = true;
 		} else {
